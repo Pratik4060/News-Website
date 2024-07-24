b446bf0e641cf4775d3e92a3179876ab791104eb
pratik <raut70856@gmail.com>
2024-07-24 19:46:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/navbar'
+import NewsBoard from './components/newsBoard'
+import NewsItem from './components/newsitem'
+
+export const App = () => {
+  const [category, setCategory] = useState("General");
+  return (
+    <div>
+      <Navbar setCategory={setCategory}/>
+      <NewsBoard category={category}/>
+    </div>
+  )
+}
+
+export default App;
